b55ce141dfc4d28ee4dd1469bb0c85eccce242a1
K-ush-agra <vashistha.kushagra22@gmail.com>
2026-04-29 22:29:14 +0530

    Remove temporary upload script

--- upload.ps1
+++
@@ -1,8 +0,0 @@
-git add .
-git commit -m "upload to github"
-git branch -M main
-git remote set-url origin https://github.com/k-ush-agra/SecureChain.git 2>$null
-if ($LASTEXITCODE -ne 0) {
-    git remote add origin https://github.com/k-ush-agra/SecureChain.git
-}
-git push -u origin main
